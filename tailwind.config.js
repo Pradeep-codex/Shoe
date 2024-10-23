971b4ac042aa7fc6fd5e5484f80b6519e47d1543
Pradeep-codex <pradeep2003c@gmail.com>
2024-10-23 14:01:11 +0530

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      screens:{
+        'm380':'380px',
+        'm500':'500px'
+      },
+    },
+  },
+  plugins: [],
+}
+
